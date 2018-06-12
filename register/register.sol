50ad0a47a2c0ec605f56d07a337ff4ecd534687d
Alex Silva <alexmsilva@gmail.com>
2018-06-12 16:38:45 -0300

    Changes the var type bytes32 to string. It's easier to explain

--- register/register.sol
+++ register/register.sol
@@ -3,30 +3,18 @@ pragma solidity ^0.4.23;
 contract Register {
 
     address public owner;
-    bytes32 private info;
+    string private info;
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function bytes32ToStr(bytes32 _bytes32) private pure returns (string) {
-        // string memory str = string(_bytes32);
-        // TypeError: Explicit type conversion not allowed from "bytes32" to "string storage pointer"
-        // thus we should fist convert bytes32 to bytes (to dynamically-sized byte array)
-        bytes memory bytesArray = new bytes(32);
-        for (uint256 i = 0; i < 32; i++) {
-            bytesArray[i] = _bytes32[i];
-        }
-
-        return string(bytesArray);
-    }
-
-    function setInfo(bytes32 _info) public {
+    function setInfo(string _info) public {
         info = _info;
     }
 
-    function getInfo() public constant returns (string) {
-        return bytes32ToStr(info);
+    function getInfo() public view returns (string) {
+        return info;
     }
 
     /*
